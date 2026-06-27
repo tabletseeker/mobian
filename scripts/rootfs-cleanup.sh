5b60de6061da8ad0b940b282f98a9c9e663df80e
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2026-06-27 00:46:50 +0000

    Preserving marvell firmware for legacy Surface devices

--- scripts/rootfs-cleanup.sh
+++ scripts/rootfs-cleanup.sh
@@ -7,24 +7,30 @@ set -e
 
 echo "Stripping server firmware out of the CORE rootfs payload..."
 rm -rf /lib/firmware/netronome /usr/lib/firmware/netronome
-rm -rf /lib/firmware/mrvl /usr/lib/firmware/mrvl
 rm -rf /lib/firmware/liquidio /usr/lib/firmware/liquidio
 rm -rf /lib/firmware/qlogic /usr/lib/firmware/qlogic
 rm -rf /lib/firmware/cxgb4 /usr/lib/firmware/cxgb4
 
-# 1. Standard Package Cleanup
+#Ensuring Wifi/Bluetooth functionality with older Surface devices
+find /lib/firmware/mrvl/ /usr/lib/firmware/mrvl/ -mindepth 1 \
+  ! -name '*8797*' \
+  ! -name '*8897*' \
+  ! -name '*8997*' \
+  ! -name 's_uapsta.bin' -delete 2>/dev/null || true
+
+# 1. package cleanup
 apt-get -y autoremove --purge
 apt-get clean
 
-# 2. CLEANING
+# 2. apt
 rm -rf /var/lib/apt/lists/*
 find /var/log -type f -exec truncate -s 0 {} +
 
-# 3. MASK SERVICES
+# 3. masking services
 systemctl mask wpa_supplicant-wired@.service
 systemctl mask wpa_supplicant-nl80211@.service
 
-# 4. ZERO FREE SPACE (Maximize XZ speed)
+# 4. zero-free
 sync
 echo "Zeroing free space for maximum compression..."
 # Using a 4k block size prevents dd from out-running the virtual disk filesystem buffer
